daa7c16a2b59470aec972ebaa174dd728fca9663
PTOM76 <ptms76@outlook.jp>
2022-04-04 01:45:52 +0900

    Fix Damage Bug

--- src/main/java/net/devtech/classicombat/mixin/regen/HungerManagerMixin.java
+++ src/main/java/net/devtech/classicombat/mixin/regen/HungerManagerMixin.java
@@ -7,10 +7,12 @@ import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(HungerManager.class)
 public class HungerManagerMixin {
+	/*
 	@Redirect(method = "update", at =@At(value = "FIELD", target = "Lnet/minecraft/entity/player/HungerManager;foodLevel:I", opcode = Opcodes.GETFIELD))
 	private int min(HungerManager manager) {
 		return 0;
 	}
+	 */
 
 	@ModifyConstant(method = "update", constant = @Constant(floatValue = 1.0F), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;canFoodHeal()Z", ordinal = 1), to = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/HungerManager;addExhaustion(F)V", ordinal = 1)))
 	private float update(float original) {
